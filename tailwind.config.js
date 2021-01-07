8c40f64b1254b67f2ec1b38ae2d18b7c7f3b9369
snakee0101 <snakee0101@gmail.com>
2021-01-07 22:00:50 +0200

    registered google colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     './resources/css/**/*.css',
   ],
   theme: {
-    extend: {}
+    extend: {
+        colors: {
+            google: {
+                'white': '#fff',
+                'red': '#f28b82',
+                'orange': '#fbbc04',
+                'yellow': '#fff475',
+                'green': '#ccff90',
+                'teal': '#a7ffeb',
+                'blue': '#cbf0f8',
+                'dark-blue': '#aecbfa',
+                'purple': '#d7aefb',
+                'pink': '#fdcfe8',
+                'brown': '#e6c9a8',
+                'grey': '#e8eaed'
+            }
+        }
+    }
   },
   variants: {},
   plugins: [
