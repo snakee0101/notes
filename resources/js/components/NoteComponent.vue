ec2471bf3d2a413e305a4f1b8ecffd195f305ee8
snakee0101 <snakee0101@gmail.com>
2021-01-07 20:49:24 +0200

    changed pin icons

--- resources/js/components/NoteComponent.vue
+++ resources/js/components/NoteComponent.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="note border border-gray-300 p-3 hover:shadow-md relative">
         <a href="" class="absolute right-1 top-1 hover:bg-gray-300 p-2 rounded-full" @click.prevent="pin()">
-            <svg class="icon icon-small icon-pushpin" viewBox="0 0 32 32">
+            <svg v-if="pinned" class="icon icon-small icon-pushpin" viewBox="0 0 32 32">
                 <path d="M17 0l-3 3 3 3-7 8h-7l5.5 5.5-8.5 11.269v1.231h1.231l11.269-8.5 5.5 5.5v-7l8-7 3 3 3-3-15-15zM14 17l-2-2 7-7 2 2-7 7z"></path>
+            </svg>
+
+            <svg v-else class="icon icon-small icon-cancel-circle" viewBox="0 0 32 32">
+                <path d="M16 0c-8.837 0-16 7.163-16 16s7.163 16 16 16 16-7.163 16-16-7.163-16-16-16zM16 29c-7.18 0-13-5.82-13-13s5.82-13 13-13 13 5.82 13 13-5.82 13-13 13z"></path>
+                <path d="M21 8l-5 5-5-5-3 3 5 5-5 5 3 3 5-5 5 5 3-3-5-5 5-5z"></path>
             </svg>
         </a>
 
