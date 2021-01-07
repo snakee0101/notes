c9c797fe468ad46e7964b94c96d350108e069df1
snakee0101 <snakee0101@gmail.com>
2021-01-07 18:48:37 +0200

    deleted paragraphs from note's content

--- resources/js/components/NoteComponent.vue
+++ resources/js/components/NoteComponent.vue
@@ -1,15 +1,24 @@
 <template>
     <div class="note border border-gray-300 p-3 hover:shadow-md">
         <h3 class="font-bold">Note's header</h3>
-        <pre class="note-content my-4 leading-6 overflow-hidden break-words">
+        <div class="note-content my-4 leading-6 overflow-hidden break-words" style="max-height: 300px">
             note's content
             note's content
             note's content
             note's content
             note's content
             note's content
-            note's c250275027520750275207520752075027502750275027520ontent</pre>
-        <div claas="toolbar">
+            note's c250275027520750275207520752075027502750275027520ontentnote's content
+            note's content
+            note's content
+            note's c250275027520750275207520752075027502750275027520ontentnote's content
+            note's content
+            note's content
+            note's c250275027520750275207520752075027502750275027520ontentnote's content
+            note's content
+            note's content
+            note's c250275027520750275207520752075027502750275027520ontent</div>
+        <div class="toolbar">
             <p>toolbar</p>
         </div>
     </div>
