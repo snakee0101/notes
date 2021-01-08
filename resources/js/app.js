96f7e602b0f3454e5c48cb6678f1d9cb3141ccf0
snakee0101 <snakee0101@gmail.com>
2021-01-08 22:53:45 +0200

    extracted collaborators dialog and added ability to hide and show it

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ require('./bootstrap');
 require('alpinejs');
 
 Vue.component('note-component', require('./components/NoteComponent.vue').default);
+Vue.component('collaborator-dialog-component', require('./components/CollaboratorDialogComponent.vue').default);
 
 new Vue({
     el: '#app'
