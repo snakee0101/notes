98812e31ecfe4f1bccf29f28c00dc3b43b43c385
snakee0101 <snakee0101@gmail.com>
2021-01-09 23:37:23 +0200

    added gray border to edit labels fields on focused state

--- resources/js/components/EditLabelsComponent.vue
+++ resources/js/components/EditLabelsComponent.vue
@@ -1,4 +1,4 @@
-<template>
+<template> <!--TODO: Доделать диалог Edit Labels-->
     <div>
         <a href="" class="p-4 rounded-r-full hover:bg-gray-200 block" @click.prevent="show()">
             <svg class="icon icon-pencil2 mr-3" viewBox="0 0 32 32">
@@ -25,7 +25,7 @@
                                     <path d="M31 12h-11v-11c0-0.552-0.448-1-1-1h-6c-0.552 0-1 0.448-1 1v11h-11c-0.552 0-1 0.448-1 1v6c0 0.552 0.448 1 1 1h11v11c0 0.552 0.448 1 1 1h6c0.552 0 1-0.448 1-1v-11h11c0.552 0 1-0.448 1-1v-6c0-0.552-0.448-1-1-1z"></path>
                                 </svg>
                             </label>
-                            <input class="add-label-input ml-4 flex-grow text-sm focus:outline-none"
+                            <input class="border-transparent border-b-2 add-label-input ml-4 flex-grow text-sm focus:outline-none focus:border-gray-200"
                                    placeholder="Create new label" required v-model="newLabel" id="newLabel"
                                    v-on:keyup.enter="addLabel(newLabel)"
                                    @focus="showCancelButton()">
@@ -44,9 +44,7 @@
                             <svg class="icon icon-xs icon-pricetag" viewBox="0 0 32 32">
                                 <path d="M30.5 0h-12c-0.825 0-1.977 0.477-2.561 1.061l-14.879 14.879c-0.583 0.583-0.583 1.538 0 2.121l12.879 12.879c0.583 0.583 1.538 0.583 2.121 0l14.879-14.879c0.583-0.583 1.061-1.736 1.061-2.561v-12c0-0.825-0.675-1.5-1.5-1.5zM23 12c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"></path>
                             </svg>
-                            <div class="ml-4 flex-grow">
-                                <input type="text" @focus="hideCancelButton()" :value="label">
-                            </div>
+                            <input type="text" @focus="hideCancelButton()" :value="label" class="border-transparent border-b-2 add-label-input ml-4 flex-grow text-sm focus:outline-none focus:border-gray-200">
                             <div class="tooltip">
                                 <a href="" class="pt-1 px-2 pb-2 rounded-full hover:bg-gray-200" @click.prevent="deleteLabel(label)">
                                     <svg class="icon icon-xs icon-close" viewBox="0 0 20 20">
