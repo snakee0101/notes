dafb8f6b2adc4afd6e7c72c09e7321911a02b2e5
snakee0101 <snakee0101@gmail.com>
2021-01-09 19:14:56 +0200

    a page can determine an active tag

--- helpers.php
+++ helpers.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 function setActiveLink($routeName) {
     return (Route::currentRouteName() == $routeName) ? 'active' : '';
 }
+
+function setActiveTagLink($tag) {
+    return (Route::currentRouteName() == 'tag') && (Route::input('tag') == $tag) ? 'active' : '';
+}
