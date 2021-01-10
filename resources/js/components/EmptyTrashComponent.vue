2b84cf622b0975a031e631d0dd1fbffb8bc0d9e4
snakee0101 <snakee0101@gmail.com>
2021-01-10 20:05:46 +0200

    made empty trash dialog styles

--- resources/js/components/EmptyTrashComponent.vue
+++ resources/js/components/EmptyTrashComponent.vue
@@ -1,8 +1,28 @@
 <template>
-    <div class="mt-4 mb-6 text-center italic ">
-        <p class="text-lg">Notes in Trash are deleted after 7 days
-            <button @click="showConfirmation()" class="ml-6 py-2 px-6 text-blue-500 text-sm font-bold hover:bg-blue-50 focus:bg-blue-100 focus:outline-none">Empty Trash</button>
-        </p>
+    <div>
+        <div class="mt-4 mb-6 text-center italic ">
+            <p class="text-lg">Notes in Trash are deleted after 7 days
+                <button @click="showConfirmation()" class="ml-6 py-2 px-6 text-blue-500 text-sm font-bold hover:bg-blue-50 focus:bg-blue-100 focus:outline-none">Empty Trash</button>
+            </p>
+        </div>
+
+        <div class="trash-confirmation fixed top-0 left-0 right-0 bottom-0 flex items-center bg-gray-800 bg-opacity-75 z-10">
+           <div class="trash-content m-auto">
+               <div class="bg-white p-6 rounded-t-lg text-center text-sm">
+                   Empty trash? All notes in Trash will be permanently deleted.
+               </div>
+               <div class="bg-white rounded-b-lg py-2 px-4 text-right">
+                   <button
+                       class="text-gray-800 text-sm font-medium px-6 py-2 mr-2 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none  rounded-sm">
+                       Cancel
+                   </button>
+                   <button
+                       class="py-2 px-6 text-blue-500 text-sm font-bold hover:bg-blue-50 focus:bg-blue-100 focus:outline-none">
+                       Empty Trash
+                   </button>
+               </div>
+           </div>
+        </div>
     </div>
 </template>
 
