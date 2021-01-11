e3c7a99742ba46af5bbed9a3cc810fd751e97b8c
snakee0101 <snakee0101@gmail.com>
2021-01-11 21:59:47 +0200

    search is started when the button is clicked

--- resources/js/components/SearchComponent.vue
+++ resources/js/components/SearchComponent.vue
@@ -1,7 +1,9 @@
 <template>
     <section class="search bg-gray-100 flex flex-grow py-4 px-3 items-center rounded-lg" :class="isSearchFieldActive ? 'active' : ''" style="max-width: 730px">
         <div class="tooltip">
-            <a href="" class="p-2 rounded-full hover:bg-gray-300">
+            <a href=""
+               class="p-2 rounded-full hover:bg-gray-300"
+               @click.prevent="search()">
                 <svg class="icon icon-small icon-search" viewBox="0 0 32 32">
                     <path d="M31.008 27.231l-7.58-6.447c-0.784-0.705-1.622-1.029-2.299-0.998 1.789-2.096 2.87-4.815 2.87-7.787 0-6.627-5.373-12-12-12s-12 5.373-12 12 5.373 12 12 12c2.972 0 5.691-1.081 7.787-2.87-0.031 0.677 0.293 1.515 0.998 2.299l6.447 7.58c1.104 1.226 2.907 1.33 4.007 0.23s0.997-2.903-0.23-4.007zM12 20c-4.418 0-8-3.582-8-8s3.582-8 8-8 8 3.582 8 8-3.582 8-8 8z"></path>
                 </svg>
@@ -28,6 +30,11 @@
 <script>
 import eventBus from "../eventBus";
 
+//TODO: When the search is started:
+//when the search button is clicked
+//When the Enter is pressed
+//While entering text
+
 export default {
     name: "SearchComponent",
     data: function () {
@@ -51,6 +58,9 @@ export default {
             this.isSearchControlsShown = false;
 
             eventBus.$emit('searchCleared');
+        },
+        search() {
+            alert('starting search');
         }
     }
 }
