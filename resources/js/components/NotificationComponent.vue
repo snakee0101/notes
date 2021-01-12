4801f561264b77f8cc97cce0ae6dbe9804c657e7
snakee0101 <snakee0101@gmail.com>
2021-01-12 22:30:18 +0200

    made notification method templates

--- resources/js/components/NotificationComponent.vue
+++ resources/js/components/NotificationComponent.vue
@@ -2,14 +2,14 @@
     <div class="notification bg-gray-800 pl-6 pr-2 py-3 text-white fixed bottom-6 left-6 rounded flex items-center">
         <p class="flex-grow">Notification</p>
         <p>
-            <button
+            <button @click="undo()"
                 class="text-yellow-300 text-sm font-medium px-6 py-2 ml-4 hover:bg-gray-700 focus:outline-none focus:bg-gray-600 rounded-sm">
                 Undo
             </button>
         </p>
         <div class="tooltip">
             <a href="" class="ml-1 p-2 rounded-full hover:bg-gray-700"
-               @click.prevent>
+               @click.prevent="dismiss()">
                 <svg class="icon icon-small icon-close" viewBox="0 0 20 20" style="fill: #fff">
                     <path
                         d="M10 8.586l-7.071-7.071-1.414 1.414 7.071 7.071-7.071 7.071 1.414 1.414 7.071-7.071 7.071 7.071 1.414-1.414-7.071-7.071 7.071-7.071-1.414-1.414-7.071 7.071z"></path>
@@ -22,7 +22,15 @@
 
 <script>
 export default {
-    name: "NotificationComponent"
+    name: "NotificationComponent",
+    methods: {
+        undo() {
+            alert('undo');
+        },
+        dismiss() {
+            alert('dismiss');
+        }
+    }
 }
 </script>
 
