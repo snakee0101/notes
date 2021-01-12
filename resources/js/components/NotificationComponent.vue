7ebf4e7e582db2d663087719417e83e9b75d6091
snakee0101 <snakee0101@gmail.com>
2021-01-12 22:57:36 +0200

    notification timeout is working. Now user can pass callback and message to notification

--- resources/js/components/NotificationComponent.vue
+++ resources/js/components/NotificationComponent.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="notification bg-gray-800 pl-6 pr-2 py-3 text-white fixed bottom-6 left-6 rounded flex items-center">
-        <p class="flex-grow">Notification</p>
+    <div class="notification bg-gray-800 pl-6 pr-2 py-3 text-white fixed bottom-6 left-6 rounded flex items-center hidden">
+        <p class="flex-grow notification-message">Notification</p>
         <p>
             <button @click="undo()"
                 class="text-yellow-300 text-sm font-medium px-6 py-2 ml-4 hover:bg-gray-700 focus:outline-none focus:bg-gray-600 rounded-sm">
@@ -21,14 +21,41 @@
 </template>
 
 <script>
+
+//TODO: Example of sending notification:    window.events.$emit('show-notification', '12345', function(){alert('test');});
+
 export default {
     name: "NotificationComponent",
+    created() {
+      window.events.$on('show-notification', function(message, undo_callback) {
+            //if there is previous notification - hide it
+            let notification = document.getElementsByClassName('notification')[0];
+            notification.classList.add('hidden');
+
+            //assign a callback
+            window.undoNotificationCallback = undo_callback;
+
+            //set message
+            let message_tag = document.getElementsByClassName('notification-message')[0];
+            message_tag.innerHTML = message;
+
+            //showNotification
+            notification.classList.remove('hidden');
+
+            //hide notification after timeout
+            setTimeout(function() {
+                let notification = document.getElementsByClassName('notification')[0];
+                notification.classList.add('hidden');
+            }, 5000);
+      });
+    },
     methods: {
         undo() {
-            alert('undo');
+            window.undoNotificationCallback();
         },
         dismiss() {
-            alert('dismiss');
+            let notification = document.getElementsByClassName('notification')[0];
+            notification.classList.add('hidden');
         }
     }
 }
