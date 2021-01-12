d21d547c756e271d68703d414e7845804470950c
snakee0101 <snakee0101@gmail.com>
2021-01-12 20:28:47 +0200

    when menu is collapsed - another styles are applied

--- resources/js/components/EditLabelsComponent.vue
+++ resources/js/components/EditLabelsComponent.vue
@@ -3,7 +3,8 @@
         <a href="" class="p-4 rounded-r-full hover:bg-gray-200 block" @click.prevent="show()">
             <svg class="icon icon-pencil2 mr-3" viewBox="0 0 32 32">
                 <path d="M12 20l4-2 14-14-2-2-14 14-2 4zM9.041 27.097c-0.989-2.085-2.052-3.149-4.137-4.137l3.097-8.525 4-2.435 12-12h-6l-12 12-6 20 20-6 12-12v-6l-12 12-2.435 4z"></path>
-            </svg> Edit labels
+            </svg>
+            <span>Edit labels</span>
         </a>
 
         <div v-if="isDialogVisible"
