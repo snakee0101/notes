38b41648b8a8422b253cf597aa7808f0fc81887b
snakee0101 <snakee0101@gmail.com>
2021-01-12 19:12:06 +0200

    made method template in menu switcher

--- resources/js/components/MenuSwitcherComponent.vue
+++ resources/js/components/MenuSwitcherComponent.vue
@@ -1,6 +1,7 @@
 <template>
     <section class="flex flex-row items-center">
-        <a href="" class="pt-2 px-2 pb-3 rounded-full hover:bg-gray-200">
+        <a href="" class="pt-2 px-2 pb-3 rounded-full hover:bg-gray-200"
+           @click.prevent="toggle()">
             <svg class="icon icon-menu" viewBox="0 0 32 32">
                 <path d="M2 6h28v6h-28zM2 14h28v6h-28zM2 22h28v6h-28z"></path>
             </svg>
@@ -12,7 +13,12 @@
 
 <script>
 export default {
-    name: "MenuSwitcher"
+    name: "MenuSwitcher",
+    methods: {
+        toggle() {
+            alert('switching menu');
+        }
+    }
 }
 </script>
 
