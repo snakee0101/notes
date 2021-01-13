467b360c1f8fa57278f3ce2be7ef3491d17332d0
snakee0101 <snakee0101@gmail.com>
2021-01-13 21:17:54 +0200

    moved notes component in the container so that is would be possible to hide it while searching

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
     <!--TODO: There should be 2 sections of the notes: PINNED and OTHERS-->
-
-    <div class="mb-10">
-        <note-component noteColor="white" :newNote="true" class="m-auto">
-
-        </note-component>
-    </div>
-
     <div class="notes-container">
+        <div class="mb-10">
+            <note-component noteColor="white" :newNote="true" class="m-auto">
+
+            </note-component>
+        </div>
+
         <note-component noteColor="orange">
 
         </note-component>
