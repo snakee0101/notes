69a76be62af7b81848fb347cfdf0051119942b9b
snakee0101 <snakee0101@gmail.com>
2021-01-20 21:53:09 +0200

    created Trash class and unit test

---
+++ tests/Unit/TrashTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class TrashTest extends TestCase
+{
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
