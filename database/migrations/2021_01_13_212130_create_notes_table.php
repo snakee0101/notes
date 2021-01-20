f6f2a0db3e8fa31dc45448e0d6a8d7679209d7a1
snakee0101 <snakee0101@gmail.com>
2021-01-20 15:10:16 +0200

    added _id column to determine note's owner

--- database/migrations/2021_01_13_212130_create_notes_table.php
+++ database/migrations/2021_01_13_212130_create_notes_table.php
@@ -15,6 +15,7 @@ class CreateNotesTable extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('owner_id');
             $table->string('header');
             $table->text('body');
             $table->boolean('pinned');
