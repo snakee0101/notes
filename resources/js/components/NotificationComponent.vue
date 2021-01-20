38fe6b5ffb1e6c4c74a0f385b8ad7f67ac7fc9b9
snakee0101 <snakee0101@gmail.com>
2021-01-20 20:51:58 +0200

    now the notification can handle undo callback properly

--- resources/js/components/NotificationComponent.vue
+++ resources/js/components/NotificationComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="notification bg-gray-800 pl-6 pr-2 py-3 text-white fixed bottom-6 left-6 rounded flex items-center hidden">
         <p class="flex-grow notification-message">Notification</p>
-        <p>
+        <p class="undo-button">
             <button @click="undo()"
                 class="text-yellow-300 text-sm font-medium px-6 py-2 ml-4 hover:bg-gray-700 focus:outline-none focus:bg-gray-600 rounded-sm">
                 Undo
@@ -26,14 +26,28 @@
 
 export default {
     name: "NotificationComponent",
+    data() {
+      return {
+          'canBeUndone': true
+      };
+    },
     created() {
       window.events.$on('show-notification', function(message, undo_callback) {
             //if there is previous notification - hide it
+            clearTimeout(window.notificationTimeout);
+
             let notification = document.getElementsByClassName('notification')[0];
             notification.classList.add('hidden');
 
             //assign a callback
+            let undo_button = document.getElementsByClassName('undo-button')[0];
+
             window.undoNotificationCallback = undo_callback;
+            if(undo_callback)
+                undo_button.classList.remove('hidden');
+            else
+                undo_button.classList.add('hidden');
+
 
             //set message
             let message_tag = document.getElementsByClassName('notification-message')[0];
@@ -43,7 +57,7 @@ export default {
             notification.classList.remove('hidden');
 
             //hide notification after timeout
-            setTimeout(function() {
+            window.notificationTimeout = setTimeout(function() {
                 let notification = document.getElementsByClassName('notification')[0];
                 notification.classList.add('hidden');
             }, 5000);
