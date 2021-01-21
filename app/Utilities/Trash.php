51685f1db0fa6475e53a1eca003f1546c51765eb
snakee0101 <snakee0101@gmail.com>
2021-01-21 14:11:44 +0200

    trash is emptied only for specific user

--- app/Utilities/Trash.php
+++ app/Utilities/Trash.php
@@ -15,6 +15,7 @@ class Trash
 
     public static function empty() {
         Note::onlyTrashed()
+            ->where('owner_id', auth()->id())
             ->get()
             ->each   //each - because it should delete collaborators, IMAGES (and its FILES), reminders and other dependencies - with the MODEL EVENTS
             ->forceDelete();
