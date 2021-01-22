92cd31f00046a370055a8aeded8f316c47bcc54c
snakee0101 <snakee0101@gmail.com>
2021-01-22 17:54:32 +0200

    made placeholder for tag page

--- resources/views/tag.blade.php
+++ resources/views/tag.blade.php
@@ -9,11 +9,20 @@
             </new-note-component>
         </div>
 
-        @foreach($notes as $note)
+        @forelse($notes as $note)
             <note-component note="{{ $note }}">
 
             </note-component>
-        @endforeach
-
+        @empty
+            <p class="text-center text-2xl mb-6 mt-20">
+                <svg class="icon icon-xl icon-price-tags" viewBox="0 0 40 32" fill="rgba(107, 114, 128, 0.2)">
+                    <path
+                        d="M38.5 0h-12c-0.825 0-1.977 0.477-2.561 1.061l-14.879 14.879c-0.583 0.583-0.583 1.538 0 2.121l12.879 12.879c0.583 0.583 1.538 0.583 2.121 0l14.879-14.879c0.583-0.583 1.061-1.736 1.061-2.561v-12c0-0.825-0.675-1.5-1.5-1.5zM31 12c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"></path>
+                    <path
+                        d="M4 17l17-17h-2.5c-0.825 0-1.977 0.477-2.561 1.061l-14.879 14.879c-0.583 0.583-0.583 1.538 0 2.121l12.879 12.879c0.583 0.583 1.538 0.583 2.121 0l0.939-0.939-13-13z"></path>
+                </svg>
+            </p>
+            <p class="text-center text-2xl text-gray-600 font-light">No notes with this label yet</p>
+        @endforelse
     </div>
 @endsection
