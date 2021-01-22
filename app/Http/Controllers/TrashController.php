6cf59769d3fe49424dd08f76c25821bf0f9ec90c
snakee0101 <snakee0101@gmail.com>
2021-01-22 14:36:34 +0200

    archived notes should be visible in trash

--- app/Http/Controllers/TrashController.php
+++ app/Http/Controllers/TrashController.php
@@ -10,7 +10,10 @@ class TrashController extends Controller
 {
     public function index() {
         return view('trash', [
-            'notes' => Note::onlyTrashed()->where('owner_id', auth()->id())->get()
+            'notes' => Note::withArchived()
+                           ->onlyTrashed()
+                           ->where('owner_id', auth()->id())
+                           ->get()
         ]);
     }
 
