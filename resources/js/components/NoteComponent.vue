b8775b94a231cae3836aa6547b3720d487305bf4
snakee0101 <snakee0101@gmail.com>
2021-01-22 13:45:41 +0200

    a note could be archived with Vue

--- resources/js/components/NoteComponent.vue
+++ resources/js/components/NoteComponent.vue
@@ -123,7 +123,7 @@
             </div>
 
             <div class="tooltip">
-                <a href="" class="hover:bg-gray-300 p-2 rounded-full" @click.prevent>
+                <a href="" class="hover:bg-gray-300 p-2 rounded-full" @click.prevent="archive()">
                     <svg class="icon icon-small icon-box-add" viewBox="0 0 32 32">
                         <path
                             d="M26 2h-20l-6 6v21c0 0.552 0.448 1 1 1h30c0.552 0 1-0.448 1-1v-21l-6-6zM16 26l-10-8h6v-6h8v6h6l-10 8zM4.828 6l2-2h18.343l2 2h-22.343z"></path>
@@ -206,6 +206,9 @@ export default {
             this.shown = true;
 
             window.events.$emit('show-notification', 'Action undone');
+        },
+        archive() {
+            axios.post('archive/' + this.note.id);
         },
         delete_forever() {
             this.shown = false;
