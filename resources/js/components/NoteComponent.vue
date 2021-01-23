04d78d1d56a172dd2cd1b9650c080d1eca57e985
snakee0101 <snakee0101@gmail.com>
2021-01-23 13:12:36 +0200

    prepared to show the labels

--- resources/js/components/NoteComponent.vue
+++ resources/js/components/NoteComponent.vue
@@ -49,6 +49,10 @@
             {{ note.body }}
         </div>
 
+        <div class="tags mb-4">
+            <a href="" class="mr-2 border border-gray-200 rounded-full px-1 py-0.5 text-sm">12345</a>
+            <a href="" class="mr-2 border border-gray-200 rounded-full px-1 py-0.5 text-sm">12345</a>
+        </div>
 
         <div class="toolbar flex justify-between" v-if="trashed">
             <button
