2b957c0bcbfe085be049161fa3792b61595bcf72
snakee0101 <snakee0101@gmail.com>
2021-01-23 21:32:46 +0200

    create 2 collaborator tests

---
+++ tests/Unit/CollaboratorTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class CollaboratorTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
