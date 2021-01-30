1ac334aed79df6da1980357111d0a1cbb0f69381
snakee0101 <snakee0101@gmail.com>
2021-01-30 21:11:11 +0200

    replaced icons in search component

--- resources/js/components/MenuSwitcherComponent.vue
+++ resources/js/components/MenuSwitcherComponent.vue
@@ -1,10 +1,7 @@
 <template>
     <section class="flex flex-row items-center">
-        <a href="" class="pt-2 px-2 pb-3 rounded-full hover:bg-gray-200"
-           @click.prevent="toggle()">
-            <svg class="icon icon-menu" viewBox="0 0 32 32">
-                <path d="M2 6h28v6h-28zM2 14h28v6h-28zM2 22h28v6h-28z"></path>
-            </svg>
+        <a href="" class="p-2 rounded-full hover:bg-gray-200" @click.prevent="toggle()">
+            <i class="bi bi-list icon-lg text-black"></i>
         </a>
         <img src="/keep.png" alt="">
         <span class="text-xl">Keep</span>
--- resources/js/components/SearchComponent.vue
+++ resources/js/components/SearchComponent.vue
@@ -1,32 +1,22 @@
 <template>
-    <section class="search bg-gray-100 flex flex-grow py-4 px-3 items-center rounded-lg" :class="isSearchFieldActive ? 'active' : ''" style="max-width: 730px">
-        <div class="tooltip">
-            <a href=""
-               class="p-2 rounded-full hover:bg-gray-300"
-               @click.prevent="search()">
-                <svg class="icon icon-small icon-search" viewBox="0 0 32 32">
-                    <path d="M31.008 27.231l-7.58-6.447c-0.784-0.705-1.622-1.029-2.299-0.998 1.789-2.096 2.87-4.815 2.87-7.787 0-6.627-5.373-12-12-12s-12 5.373-12 12 5.373 12 12 12c2.972 0 5.691-1.081 7.787-2.87-0.031 0.677 0.293 1.515 0.998 2.299l6.447 7.58c1.104 1.226 2.907 1.33 4.007 0.23s0.997-2.903-0.23-4.007zM12 20c-4.418 0-8-3.582-8-8s3.582-8 8-8 8 3.582 8 8-3.582 8-8 8z"></path>
-                </svg>
-            </a>
-            <span class="tooltiptext">Search</span>
-        </div>
+    <section class="search bg-gray-100 flex flex-grow p-2 items-center rounded-lg"
+             :class="isSearchFieldActive ? 'active' : ''" style="max-width: 730px">
+        <a href="" class="p-2 rounded-full hover:bg-gray-300"
+           @click.prevent="search()">
+            <i class="bi bi-search icon text-black"></i>
+        </a><!--TODO: Restore the tooltip (Search)-->
         <input type="text" class="flex-grow focus:outline-none mx-2 bg-gray-100" placeholder="Search"
                @focus="inFocus()"
-               @blur="inBlur()"
+               @blur="isSearchFieldActive = false"
                @keypress.enter="search()"
                @input="delayedSearch()"
                v-model="searchText">
-        <div class="tooltip">
-            <a href="" class="p-2 rounded-full hover:bg-gray-300"
-               v-if="isSearchControlsShown"
-               @click.prevent="clear()">
-                <svg class="icon icon-small icon-close" viewBox="0 0 20 20">
-                    <path d="M10 8.586l-7.071-7.071-1.414 1.414 7.071 7.071-7.071 7.071 1.414 1.414 7.071-7.071 7.071 7.071 1.414-1.414-7.071-7.071 7.071-7.071-1.414-1.414-7.071 7.071z"></path>
-                </svg>
-            </a>
-            <span class="tooltiptext">Clear search</span>
-        </div>
-
+        <a href="" class="p-1 rounded-full hover:bg-gray-300"
+           v-if="isSearchControlsShown"
+           @click.prevent="clear()">
+            <i class="bi bi-x icon-lg text-black"></i>
+        </a>
+        <!--TODO: Restore the tooltip (Clear search)-->
     </section>
 </template>
 
@@ -47,21 +37,15 @@ export default {
 
             window.events.$emit('searchActivated');
         },
-        inBlur() {
-            this.isSearchFieldActive = false;
-        },
-        clearSearchText() {
-            this.searchText = '';
-        },
         clear() {
             this.isSearchFieldActive = false;
             this.isSearchControlsShown = false;
 
-            this.clearSearchText();
+            this.searchText = '';
             window.events.$emit('searchCleared');
         },
         search() {
-            if(this.searchText !== '')
+            if (this.searchText !== '')
                 alert('starting search');
         },
         delayedSearch() {
