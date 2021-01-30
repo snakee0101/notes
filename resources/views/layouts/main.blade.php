bf8e31da748cf26c0f81dd78b5e9d7a279e2e6c8
snakee0101 <snakee0101@gmail.com>
2021-01-30 20:20:47 +0200

    imported bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,7 +19,6 @@
 <body class="h-screen antialiased leading-none font-sans flex flex-col">
 <div id="app">
     <header class="flex flex-row justify-between py-2 px-3 shadow-sm items-center">
-
         <menu-switcher-component></menu-switcher-component>
         <search-component></search-component>
 
