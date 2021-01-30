a7964932b3b7dc3bbc1f7066191f5b5e2c5cada8
snakee0101 <snakee0101@gmail.com>
2021-01-30 22:21:53 +0200

    made some of the Note toolbar buttons and tooltips

--- resources/js/components/NoteComponent.vue
+++ resources/js/components/NoteComponent.vue
@@ -79,7 +79,7 @@
         <div class="toolbar" v-else>
             <div class="tooltip reminders-dropdown-tooltip">
                 <a href="" class="hover:bg-gray-300 p-2 rounded-full" @click.prevent="showRemindersDropdown()">
-                    <svg class="icon icon-small icon-bell" viewBox="0 0 32 32">
+                    <svg class="icon icon-sm icon-bell" viewBox="0 0 32 32">
                         <path
                             d="M32.047 25c0-9-8-7-8-14 0-0.58-0.056-1.076-0.158-1.498-0.526-3.532-2.88-6.366-5.93-7.23 0.027-0.123 0.041-0.251 0.041-0.382 0-1.040-0.9-1.891-2-1.891s-2 0.851-2 1.891c0 0.131 0.014 0.258 0.041 0.382-3.421 0.969-5.966 4.416-6.039 8.545-0.001 0.060-0.002 0.121-0.002 0.183 0 7-8 5-8 14 0 2.382 5.331 4.375 12.468 4.878 0.673 1.263 2.002 2.122 3.532 2.122s2.86-0.86 3.532-2.122c7.137-0.503 12.468-2.495 12.468-4.878 0-0.007-0.001-0.014-0.001-0.021l0.048 0.021zM25.82 26.691c-1.695 0.452-3.692 0.777-5.837 0.958-0.178-2.044-1.893-3.648-3.984-3.648s-3.805 1.604-3.984 3.648c-2.144-0.18-4.142-0.506-5.837-0.958-2.332-0.622-3.447-1.318-3.855-1.691 0.408-0.372 1.523-1.068 3.855-1.691 2.712-0.724 6.199-1.122 9.82-1.122s7.109 0.398 9.82 1.122c2.332 0.622 3.447 1.318 3.855 1.691-0.408 0.372-1.523 1.068-3.855 1.691z"></path>
                     </svg>
@@ -121,18 +121,18 @@
                v-b-tooltip.hover.bottom
                title="Collaborator"
                @click.prevent="showCollaboratorsDialog()">
-                <i class="bi bi-person-plus icon"></i>
+                <i class="bi bi-person-plus icon-sm"></i>
             </a>
 
-            <div class="tooltip">
-                <a href="" class="hover:bg-gray-300 p-2 rounded-full" @click.prevent>
-                    <svg class="icon icon-small icon-palette" viewBox="0 0 24 24">
-                        <path
-                            d="M17.484 12q0.609 0 1.055-0.422t0.445-1.078-0.445-1.078-1.055-0.422-1.055 0.422-0.445 1.078 0.445 1.078 1.055 0.422zM14.484 8.016q0.609 0 1.055-0.445t0.445-1.055-0.445-1.055-1.055-0.445-1.055 0.445-0.445 1.055 0.445 1.055 1.055 0.445zM9.516 8.016q0.609 0 1.055-0.445t0.445-1.055-0.445-1.055-1.055-0.445-1.055 0.445-0.445 1.055 0.445 1.055 1.055 0.445zM6.516 12q0.609 0 1.055-0.422t0.445-1.078-0.445-1.078-1.055-0.422-1.055 0.422-0.445 1.078 0.445 1.078 1.055 0.422zM12 3q3.703 0 6.352 2.344t2.648 5.672q0 2.063-1.477 3.516t-3.539 1.453h-1.734q-0.656 0-1.078 0.445t-0.422 1.055q0 0.516 0.375 0.984t0.375 1.031q0 0.656-0.422 1.078t-1.078 0.422q-3.75 0-6.375-2.625t-2.625-6.375 2.625-6.375 6.375-2.625z"></path>
-                    </svg>
-                </a>
-                <span class="tooltiptext">Change color</span>
-                <div class="vertical-tooltiptext rounded-md"> <!--TODO: Color circles' tooltips work another way - they're smaller and they're closed when the cursor is out of the color circle (even when it was on the tooltip). So when the cursor is on the tooltip, it will be closed.-->
+            <a href="" class="hover:bg-gray-300 p-2 rounded-full"
+               v-b-tooltip.hover.bottom
+               title="Change color"
+               @click.prevent>
+                <i class="bi bi-palette icon-sm"></i>
+            </a>
+
+<!--            <div class="tooltip">   TODO: Problems with color circles
+                <div class="vertical-tooltiptext rounded-md"> &lt;!&ndash;TODO: Color circles' tooltips work another way - they're smaller and they're closed when the cursor is out of the color circle (even when it was on the tooltip). So when the cursor is on the tooltip, it will be closed.&ndash;&gt;
                     <div class="tooltip2" v-for="color in colors">
                         <a href=""
                            :class="'color-circle border-2 transition border-transparent p-2 m-1 d-inline-block rounded-full bg-google-' + color + ' ' + isActive(color)"
@@ -144,14 +144,14 @@
                         <span class="tooltip2text" v-text="color"></span>
                     </div>
                 </div>
-            </div>
+            </div>-->
 
 
             <a href="" class="hover:bg-gray-300 p-2 rounded-full"
                v-b-tooltip.hover.bottom
                title="Add image"
                @click.prevent="selectImage()">
-                <i class="bi bi-image icon"></i>
+                <i class="bi bi-image icon-sm"></i>
             </a>
 
             <input type="file" ref="image" class="hidden" accept="image/jpeg,image/png,image/gif"
@@ -163,7 +163,7 @@
                title="Unarchive"
                @click.prevent="unarchive()"
                v-if="note.archived">
-                <i class="bi bi-save2-fill icon"></i>
+                <i class="bi bi-save2-fill icon-sm"></i>
             </a>
 
             <a href="" class="hover:bg-gray-300 p-2 rounded-full"
@@ -171,7 +171,7 @@
                title="Archive"
                @click.prevent="archive()"
                v-else>
-                <i class="bi bi-save2-fill icon"></i>
+                <i class="bi bi-save2-fill icon-sm"></i>
             </a>
 
             <div class="tooltip dropdown-tooltip">
