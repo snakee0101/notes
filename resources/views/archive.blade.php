89d32b9e606b63964677ad12af6d6addf8d121aa
snakee0101 <snakee0101@gmail.com>
2021-01-31 16:06:42 +0200

    replaced page placeholder icons

--- resources/views/archive.blade.php
+++ resources/views/archive.blade.php
@@ -8,10 +8,7 @@
             </note-component>
         @empty
             <p class="text-center text-2xl mb-6 mt-14">
-                <svg class="icon icon-xl icon-box-add" viewBox="0 0 32 32" fill="rgba(107, 114, 128, 0.2)">
-                    <path
-                        d="M26 2h-20l-6 6v21c0 0.552 0.448 1 1 1h30c0.552 0 1-0.448 1-1v-21l-6-6zM16 26l-10-8h6v-6h8v6h6l-10 8zM4.828 6l2-2h18.343l2 2h-22.343z"></path>
-                </svg>
+                <i class="bi bi-save2-fill icon-xl"></i>
             </p>
             <p class="text-center text-2xl text-gray-600 font-light">Your archived notes appear here</p>
         @endforelse
--- resources/views/trash.blade.php
+++ resources/views/trash.blade.php
@@ -8,13 +8,8 @@
 
             </note-component>
         @empty
-            <p class="text-center text-2xl mb-6 mt-20">
-                <svg class="icon icon-xl icon-bin" viewBox="0 0 32 32" fill="rgba(107, 114, 128, 0.2)">
-                    <path
-                        d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>
-                    <path
-                        d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>
-                </svg>
+            <p class="text-center text-2xl mb-6 mt-14">
+                <i class="bi bi-trash-fill icon-xl"></i>
             </p>
             <p class="text-center text-2xl text-gray-600 font-light">No notes in Trash</p>
         @endforelse
