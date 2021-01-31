d0931033ca9390fcb4266b59bdd288e7420a8d28
snakee0101 <snakee0101@gmail.com>
2021-01-31 18:49:47 +0200

    simplified some notification markup

--- resources/js/components/NotificationComponent.vue
+++ resources/js/components/NotificationComponent.vue
@@ -1,22 +1,17 @@
 <template>
-    <div class="notification bg-gray-800 pl-6 pr-2 py-3 text-white fixed bottom-6 left-6 rounded flex items-center hidden">
-        <p class="flex-grow notification-message">Notification</p>
-        <p class="undo-button">
+    <div class="notification bg-gray-800 pl-6 pr-2 py-1.5 text-white fixed bottom-6 left-6 rounded flex items-center hidden">
+        <p class="flex-grow notification-message m-0">Notification</p>
+        <p class="undo-button m-0">
             <button @click="undo()"
-                class="text-yellow-300 text-sm font-medium px-6 py-2 ml-4 hover:bg-gray-700 focus:outline-none focus:bg-gray-600 rounded-sm">
+                    class="text-yellow-300 text-sm font-medium px-6 py-2 ml-4 hover:bg-gray-700 focus:outline-none focus:bg-gray-600 rounded-sm">
                 Undo
             </button>
         </p>
-        <div class="tooltip">
-            <a href="" class="ml-1 p-2 rounded-full hover:bg-gray-700"
-               @click.prevent="dismiss()">
-                <svg class="icon icon-small icon-close" viewBox="0 0 20 20" style="fill: #fff">
-                    <path
-                        d="M10 8.586l-7.071-7.071-1.414 1.414 7.071 7.071-7.071 7.071 1.414 1.414 7.071-7.071 7.071 7.071 1.414-1.414-7.071-7.071 7.071-7.071-1.414-1.414-7.071 7.071z"></path>
-                </svg>
-            </a>
-            <span class="tooltiptext">Dismiss</span>
-        </div>
+        <a href="" class="ml-1 p-2 rounded-full hover:bg-gray-700"
+           v-b-tooltip.hover.bottom title="Dismiss"
+           @click.prevent="dismiss()">
+            <i class="bi bi-x icon-lg text-white"></i>
+        </a>
     </div>
 </template>
 
@@ -27,12 +22,12 @@
 export default {
     name: "NotificationComponent",
     data() {
-      return {
-          'canBeUndone': true
-      };
+        return {
+            'canBeUndone': true
+        };
     },
     created() {
-      window.events.$on('show-notification', function(message, undo_callback) {
+        window.events.$on('show-notification', function (message, undo_callback) {
             //if there is previous notification - hide it
             clearTimeout(window.notificationTimeout);
 
@@ -43,7 +38,7 @@ export default {
             let undo_button = document.getElementsByClassName('undo-button')[0];
 
             window.undoNotificationCallback = undo_callback;
-            if(undo_callback)
+            if (undo_callback)
                 undo_button.classList.remove('hidden');
             else
                 undo_button.classList.add('hidden');
@@ -57,11 +52,11 @@ export default {
             notification.classList.remove('hidden');
 
             //hide notification after timeout
-            window.notificationTimeout = setTimeout(function() {
+            window.notificationTimeout = setTimeout(function () {
                 let notification = document.getElementsByClassName('notification')[0];
                 notification.classList.add('hidden');
             }, 5000);
-      });
+        });
     },
     methods: {
         undo() {
