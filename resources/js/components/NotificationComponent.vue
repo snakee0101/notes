9bfbb0b5b6efa410efcf562ccb2b4059f49ef2ad
snakee0101 <snakee0101@gmail.com>
2021-01-31 19:17:24 +0200

    rebuilt NotificationComponent according to the data-driven principle

--- resources/js/components/NotificationComponent.vue
+++ resources/js/components/NotificationComponent.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="notification bg-gray-800 pl-6 pr-2 py-1.5 text-white fixed bottom-6 left-6 rounded flex items-center hidden">
-        <p class="flex-grow notification-message m-0">Notification</p>
-        <p class="undo-button m-0">
+    <div class="notification bg-gray-800 pl-6 pr-2 py-1.5 text-white fixed bottom-6 left-6 rounded flex items-center"
+    :class="isHidden ? 'hidden' : ''">
+        <p class="flex-grow notification-message m-0">{{ message }}</p>
+        <p class="undo-button m-0" :class="isUndoHidden ? 'hidden' : ''">
             <button @click="undo()" class="notification_undo_button">
                 Undo
             </button>
         </p>
         <a href="" class="ml-1 p-2 rounded-full hover:bg-gray-700"
            v-b-tooltip.hover.bottom title="Dismiss"
-           @click.prevent="dismiss()">
+           @click.prevent="hide()">
                 <i class="bi bi-x icon-lg text-white"></i>
         </a>
     </div>
@@ -22,48 +23,32 @@ export default {
     name: "NotificationComponent",
     data() {
         return {
-            'canBeUndone': true
+            'message' : '',
+            'canBeUndone' : true,
+            'isHidden' : true,
+            'isUndoHidden' : true,
+            'undo' : function(){}
         };
     },
     created() {
-        window.events.$on('show-notification', function (message, undo_callback) {
-            //if there is previous notification - hide it
-            clearTimeout(window.notificationTimeout);
-
-            let notification = document.getElementsByClassName('notification')[0];
-            notification.classList.add('hidden');
-
-            //assign a callback
-            let undo_button = document.getElementsByClassName('undo-button')[0];
-
-            window.undoNotificationCallback = undo_callback;
-            if (undo_callback)
-                undo_button.classList.remove('hidden');
-            else
-                undo_button.classList.add('hidden');
-
-
-            //set message
-            let message_tag = document.getElementsByClassName('notification-message')[0];
-            message_tag.innerHTML = message;
-
-            //showNotification
-            notification.classList.remove('hidden');
-
-            //hide notification after timeout
-            window.notificationTimeout = setTimeout(function () {
-                let notification = document.getElementsByClassName('notification')[0];
-                notification.classList.add('hidden');
-            }, 5000);
-        });
+        window.events.$on('show-notification', this.show);
     },
     methods: {
-        undo() {
-            window.undoNotificationCallback();
+        show(message, undo_callback)
+        {
+            clearTimeout(window.notificationTimeout);  //if there is previous notification - hide it
+            this.isHidden = true;
+
+            this.undo = undo_callback;  //assign a callback
+            this.isUndoHidden = !undo_callback;
+
+            this.message = message;
+            this.isHidden = false;
+
+            window.notificationTimeout = setTimeout(this.hide, 5000);
         },
-        dismiss() {
-            let notification = document.getElementsByClassName('notification')[0];
-            notification.classList.add('hidden');
+        hide() {
+            this.isHidden = true;
         }
     }
 }
