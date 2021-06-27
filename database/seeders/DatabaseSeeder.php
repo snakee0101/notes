107182d8f0e11a4c978c3ff5abb6bc6428db313e
snakee0101 <snakee0101@gmail.com>
2021-06-27 17:43:31 +0300

    created test notes seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = \App\Models\User::find(1);
+        \App\Models\Note::factory()->for($user,'owner')->count(1000)->create();
     }
 }
