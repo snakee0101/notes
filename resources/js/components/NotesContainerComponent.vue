30d855b27adb55b845f655dd78f8726be1ecf9ff
snakee0101 <snakee0101@gmail.com>
2021-06-28 22:37:46 +0300

    added scrolling to top when proceeding to the next/prev page

--- resources/js/components/NotesContainerComponent.vue
+++ resources/js/components/NotesContainerComponent.vue
@@ -3,7 +3,7 @@
         <div class="pinned">
             <p class="font-bold text-sm mb-2">PINNED</p>
 
-            <div v-masonry transition-duration="0.3s" item-selector=".note" gutter=".gutter" :origin-top="true">
+            <div v-masonry transition-duration="0.3s" item-selector=".note" gutter=".gutter" :origin-top="true" id="pinned_notes_content">
                 <div class="gutter"></div>
 
                 <note-component v-masonry-tile
@@ -24,7 +24,7 @@
         <div class="others">
             <p class="font-bold text-sm mt-20 mb-2">OTHERS</p>
 
-            <div v-masonry transition-duration="0.3s" item-selector=".note" gutter=".gutter" :origin-top="true">
+            <div v-masonry transition-duration="0.3s" item-selector=".note" gutter=".gutter" :origin-top="true" id="other_notes_content">
                 <div class="gutter"></div>
 
                 <note-component v-masonry-tile
@@ -98,6 +98,9 @@ export default {
             this.pinned_notes_paginator = res.data;
 
             res.data.data.forEach( (value, index) => this.$set(this.pinned_notes_collection, index, value) ); //force refresh of the paginator to make it reactive
+
+            let contentHeight = document.getElementById('pinned_notes_content').clientHeight;
+            window.scrollBy(0,-contentHeight/2);
         },
 
 
@@ -117,6 +120,9 @@ export default {
             this.other_notes_paginator = res.data;
 
             res.data.data.forEach( (value, index) => this.$set(this.other_notes_collection, index, value) ); //force refresh of the paginator to make it reactive
+
+            let contentHeight = document.getElementById('other_notes_content').clientHeight;
+            window.scrollBy(0,-contentHeight/2);
         }
     }
 }
