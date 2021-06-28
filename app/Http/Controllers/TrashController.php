14fda0daf17506b9a12d1c6ddc176402bf25c847
snakee0101 <snakee0101@gmail.com>
2021-06-28 18:14:36 +0300

    fixed notes pagination on Trash page

--- app/Http/Controllers/TrashController.php
+++ app/Http/Controllers/TrashController.php
@@ -9,17 +9,25 @@ use Illuminate\Http\Request;
 class TrashController extends Controller
 {
     public function index() {
-        return view('trash', [
-            "pinned_notes" => auth()->user()->notes()->withArchived()
-            ->onlyTrashed()
-            ->where('pinned', true)
-            ->paginate()->toJson(),
-
+        $data = [
+            'pinned_notes' => auth()->user()->notes()->withArchived()
+                ->onlyTrashed()
+                ->where('pinned', true)
+                ->paginate(),
             'other_notes' => auth()->user()->notes()->withArchived()
-            ->onlyTrashed()
-            ->where('pinned', false)
-            ->paginate()->toJson()
-        ]);
+                ->onlyTrashed()
+                ->where('pinned', false)
+                ->paginate()
+        ];
+
+        if(! request()->wantsJson()) {
+            $data['pinned_notes'] = $data['pinned_notes']->toJson();
+            $data['other_notes'] = $data['other_notes']->toJson();
+
+            return view('trash', $data);
+        }
+
+        return $data[ request('notes_type') ];
     }
 
     public function empty() {
