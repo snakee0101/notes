d78b67a5a4d625bbc7b72d64795b1dee5c4601df
snakee0101 <snakee0101@gmail.com>
2021-06-28 23:20:58 +0300

    NotesContainerComponent - made trash page placeholder

--- resources/js/components/NotesContainerComponent.vue
+++ resources/js/components/NotesContainerComponent.vue
@@ -66,6 +66,13 @@
                 </p>
                 <p class="text-center text-2xl text-gray-600 font-light">Your archived notes appear here</p>
             </div>
+
+            <div v-if="isOnPage('/trash')">
+                <p class="text-center text-2xl mb-6 mt-14">
+                    <i class="bi bi-trash-fill icon-xl"></i>
+                </p>
+                <p class="text-center text-2xl text-gray-600 font-light">No notes in Trash</p>
+            </div>
         </div>
     </div>
 </template>
--- resources/views/trash.blade.php
+++ resources/views/trash.blade.php
@@ -3,16 +3,9 @@
 @section('content')
     <empty-trash-component></empty-trash-component>
 
-{{--    @if( ! $notes->isEmpty() )--}}
-        <notes-container-component :pinned_notes="{{ $pinned_notes }}"
-                                   :other_notes="{{ $other_notes }}"
-                                   :is-trashed="true">
+    <notes-container-component :pinned_notes="{{ $pinned_notes }}"
+                               :other_notes="{{ $other_notes }}"
+                               :is-trashed="true">
 
-        </notes-container-component>
-{{--    @else--}}
-{{--        <p class="text-center text-2xl mb-6 mt-14">
-            <i class="bi bi-trash-fill icon-xl"></i>
-        </p>
-        <p class="text-center text-2xl text-gray-600 font-light">No notes in Trash</p>
-    @endif--}}
+    </notes-container-component>
 @endsection
