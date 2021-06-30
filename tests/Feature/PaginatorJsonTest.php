d9e7c2c2b9c9ef87be83232aac04712a3a44923c
snakee0101 <snakee0101@gmail.com>
2021-06-30 13:02:00 +0300

    pagination test - checked archive page JSON response

--- tests/Feature/PaginatorJsonTest.php
+++ tests/Feature/PaginatorJsonTest.php
@@ -11,7 +11,6 @@ use Tests\TestCase;
 class PaginatorJsonTest extends TestCase
 {
 
- // /archive
  // /tag
  // /reminder
 
@@ -34,6 +33,29 @@ class PaginatorJsonTest extends TestCase
 
 
         $http_response = $this->getJson( route('notes') . '?page=2&notes_type=other_notes' );
+        $this->assertJson($http_response->content());
+
+        $this->assertStringContainsString("\"header\":\"{$notes->last()->header}\"", $http_response->content());
+        $this->assertStringNotContainsString("\"header\":\"{$notes->first()->header}\"", $http_response->content());
+    }
+
+    public function test_archive_page_paginator()
+    {
+        $owner = User::factory()->create();
+        $notes = Note::factory(['archived' => true])->for($owner,'owner')
+            ->count(40)
+            ->create();
+
+        auth()->login($owner);
+
+        $http_response = $this->getJson( route('archive') . '?page=1&notes_type=other_notes' );
+        $this->assertJson($http_response->content());
+
+        $this->assertStringContainsString("\"header\":\"{$notes->first()->header}\"", $http_response->content());
+        $this->assertStringNotContainsString("\"header\":\"{$notes->last()->header}\"", $http_response->content());
+
+
+        $http_response = $this->getJson( route('archive') . '?page=2&notes_type=other_notes' );
         $this->assertJson($http_response->content());
 
         $this->assertStringContainsString("\"header\":\"{$notes->last()->header}\"", $http_response->content());
