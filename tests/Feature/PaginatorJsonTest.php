b5758cee812155142d9c672cef15e100be3c6083
snakee0101 <snakee0101@gmail.com>
2021-06-30 12:59:13 +0300

    pagination test - checked trash page JSON response

--- tests/Feature/PaginatorJsonTest.php
+++ tests/Feature/PaginatorJsonTest.php
@@ -11,10 +11,11 @@ use Tests\TestCase;
 class PaginatorJsonTest extends TestCase
 {
 
- // /trash
  // /archive
  // /tag
  // /reminder
+
+    //TODO: there is no pinned/OTHER NOTES COLLECTION CHECK
 
     public function test_main_page_paginator()
     {
@@ -33,6 +34,33 @@ class PaginatorJsonTest extends TestCase
 
 
         $http_response = $this->getJson( route('notes') . '?page=2&notes_type=other_notes' );
+        $this->assertJson($http_response->content());
+
+        $this->assertStringContainsString("\"header\":\"{$notes->last()->header}\"", $http_response->content());
+        $this->assertStringNotContainsString("\"header\":\"{$notes->first()->header}\"", $http_response->content());
+    }
+
+    public function test_trash_paginator()
+    {
+        $owner = User::factory()->create();
+        $notes = Note::factory()->for($owner,'owner')
+            ->count(40)
+            ->create();
+
+        $notes->each->delete();
+
+        $this->assertNotEmpty(Note::onlyTrashed()->get());
+
+        auth()->login($owner);
+
+        $http_response = $this->getJson( route('trash') . '?page=1&notes_type=other_notes' );
+        $this->assertJson($http_response->content());
+
+        $this->assertStringContainsString("\"header\":\"{$notes->first()->header}\"", $http_response->content());
+        $this->assertStringNotContainsString("\"header\":\"{$notes->last()->header}\"", $http_response->content());
+
+
+        $http_response = $this->getJson( route('trash') . '?page=2&notes_type=other_notes' );
         $this->assertJson($http_response->content());
 
         $this->assertStringContainsString("\"header\":\"{$notes->last()->header}\"", $http_response->content());
