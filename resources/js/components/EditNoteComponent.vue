6bf77f3cdfef96998db74163a1458033cf8e8639
snakee0101 <snakee0101@gmail.com>
2021-07-01 16:32:44 +0300

    created dialog that responds to clicking on the note

--- resources/js/components/EditNoteComponent.vue
+++ resources/js/components/EditNoteComponent.vue
@@ -1,10 +1,26 @@
 <template>
+    <b-modal id="edit-note-modal" title="Edit note" ref="edit-note-modal" centered v-on:ok="applyChanges()" v-on:cancel="cancel()">
 
+    </b-modal>
 </template>
 
 <script>
 export default {
-name: "edit-note-component"
+    name: "edit-note-component",
+    created() {
+        window.events.$on('open_note_for_editing', this.openModal);
+    },
+    methods: {
+        openModal() {
+            this.$refs['edit-note-modal'].show();
+        },
+        applyChanges() {
+            console.log('apply');
+        },
+        cancel() {
+            console.log('cancel');
+        }
+    }
 }
 </script>
 
