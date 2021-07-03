5b3e2a26daa02e9abc4ba12373f91465d4805680
snakee0101 <snakee0101@gmail.com>
2021-07-03 12:33:07 +0300

    EditNoteComponent - header and content of the note are displayed in dialog

--- resources/js/components/EditNoteComponent.vue
+++ resources/js/components/EditNoteComponent.vue
@@ -3,18 +3,34 @@
              v-on:ok="applyChanges()"
              v-on:cancel="cancel()"
              modal-class="edit-note-modal">
-        <note-component :note="note" :isEditing="true" style="width: 630px">
 
-        </note-component>
+        <div class="p-4">
+            <h3 class="pb-3">Edit note</h3>
+
+            <h6 class="pb-1">Header</h6>
+            <textarea name="note_header" placeholder="Title"
+                      class="note-header-input mx-2 focus:outline-none h-auto resize-none font-bold bg-transparent text-xl"
+                      v-model="header">
+
+            </textarea>
+
+            <h6 class="pb-1">Note content</h6>
+            <div>
+                <input type="hidden" id="note_content" v-model="note.body">
+                <trix-editor input="note_content" ref="note-editor" @change="note.body = $event.target.value"></trix-editor>
+            </div>
+        </div>
     </b-modal>
 </template>
 
 <script>
 export default {
-    name: "edit-note-component",
+    name: "EditNoteComponent",
     data() {
         return {
-            note: {}
+            note: {},
+            header: '',
+            body: ''
         };
     },
     created() {
@@ -22,10 +38,14 @@ export default {
     },
     methods: {
         openModal(note) {
-            this.note = note;
-            this.$refs['edit-note-modal'].show();
+            this.note = JSON.parse(JSON.stringify(note));
+            this.header = this.note.header;
+            this.body = this.note.body;
+
+            this.$refs["edit-note-modal"].show();
         },
         applyChanges() {
+            this.body = this.$refs['note-editor'].value;
             console.log('apply');
         },
         cancel() {
