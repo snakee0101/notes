1b30f0db4d1b80b72386f9c6849b35d72d917974
snakee0101 <snakee0101@gmail.com>
2021-07-15 16:06:06 +0300

    applied Checklist::wrap method to ChecklistController::store

--- app/Http/Controllers/ChecklistController.php
+++ app/Http/Controllers/ChecklistController.php
@@ -4,27 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Checklist;
 use App\Models\Note;
-use App\Models\Task;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
 
 class ChecklistController extends Controller
 {
 
     public function store(Request $request) //Only converts text to a list
     {
-        $collection = new Collection($request->checklist_data);
-
         $note = Note::findOrFail($request->note_id); //Checklist could be created only for existing note
         $checklist = $note->checklist()->create();
 
-        $wrapped = $collection->map(fn($task, $index) => [ //wrap task text into array and assign it a position according to its index
-            'text' => $task['text'],
-            'position' => $index + 1,
-            'completed' => $task['completed']
-        ]);
-
-        $checklist->tasks()->createMany($wrapped); //Save all tasks to the checklist
+        $checklist->tasks()->createMany( Checklist::wrap($request->checklist_data) ); //Save all tasks to the checklist
 
         return $note->fresh();
     }
