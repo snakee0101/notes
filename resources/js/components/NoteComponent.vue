c726a8b01694ddde85a0401cc4d07b871265a839
Danil <snakee0101@gmail.com>
2021-07-27 17:13:17 +0300

    NoteComponent - HTML- is filled with real links data

--- resources/js/components/NoteComponent.vue
+++ resources/js/components/NoteComponent.vue
@@ -201,15 +201,17 @@
             </a>
         </div>
 
-        <div v-for="link in [1,2,3]" class="mt-1">
+        <div v-for="link in note.links" class="mt-1">
             <div class="flex flex-row mt-2 items-center">
-                <img style="height: 40px; width: 40px" src="https://images.unsplash.com/photo-1550853024-fae8cd4be47f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJpcmR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" alt="">
+                <img style="height: 40px; width: 40px"
+                     :src="link.favicon_path" alt="">
                 <div class="ml-2 flex-grow">
-                    <h5 class="m-0">site name</h5>
-                    <p class="text-sm m-0 text-gray-600">site domain</p>
+                    <h5 class="m-0">{{ link.name }}</h5>
+                    <p class="text-sm m-0 text-gray-600">{{ link.domain }}</p>
                 </div>
                 <div>
-                    <a href=""
+                    <a :href="link.url"
+                       target="_blank"
                        v-b-tooltip.hover.bottom
                        title="Go to link">
                         <i class="bi bi-box-arrow-up-right"></i>
