ac5ea66020f0d489757eadb95198da53c496f78f
Danil <snakee0101@gmail.com>
2021-08-03 09:44:20 +0300

    fixed problems with displaying notes in the container

--- resources/views/archive.blade.php
+++ resources/views/archive.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.main')
 
 @section('content')
+    <div class="notes-container">
         <notes-container-component :pinned_notes="{{ $pinned_notes }}"
                                    :other_notes="{{ $other_notes }}">
 
         </notes-container-component>
+    </div>
 @endsection
--- resources/views/trash.blade.php
+++ resources/views/trash.blade.php
@@ -3,9 +3,11 @@
 @section('content')
     <empty-trash-component></empty-trash-component>
 
-    <notes-container-component :pinned_notes="{{ $pinned_notes }}"
-                               :other_notes="{{ $other_notes }}"
-                               :is-trashed="true">
+    <div class="notes-container">
+        <notes-container-component :pinned_notes="{{ $pinned_notes }}"
+                                   :other_notes="{{ $other_notes }}"
+                                   :is-trashed="true">
 
-    </notes-container-component>
+        </notes-container-component>
+    </div>
 @endsection
