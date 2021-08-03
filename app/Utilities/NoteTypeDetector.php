9864a1ffc6319781d59e61daf73ba4f18e5b261c
Danil <snakee0101@gmail.com>
2021-08-03 20:16:56 +0300

    NoteTypeDetector could be initialized statically

--- app/Utilities/NoteTypeDetector.php
+++ app/Utilities/NoteTypeDetector.php
@@ -6,5 +6,15 @@ use App\Models\Note;
 
 class NoteTypeDetector
 {
+    private Note $note;
 
+    public function __construct(Note $note)
+    {
+        $this->note = $note;
+    }
+
+    public static function select(Note $note) : static
+    {
+        return new static($note);
+    }
 }
--- tests/Unit/NoteTypeDetectorTest.php
+++ tests/Unit/NoteTypeDetectorTest.php
@@ -2,13 +2,29 @@
 
 namespace Tests\Unit;
 
+use \App\Models\{Note, Image};
+use App\Utilities\NoteTypeDetector;
+
 use Tests\TestCase;
 
 class NoteTypeDetectorTest extends TestCase
 {
-    public function test_note_type_detector_detects_images()
+    public $empty_note, $note_with_images;
+
+    protected function setUp(): void
     {
-        $empty_note = Note::factory()->create();
-        $note_with_images = Note::factory()->has( Image::factory() )->create();
+        parent::setUp();
+        $this->empty_note = Note::factory()->create();
+        $this->note_with_images = Note::factory()->has( Image::factory() )->create();
     }
+
+    public function test_note_type_detector_could_be_initialized_statically()
+    {
+        $this->assertInstanceOf(NoteTypeDetector::class, NoteTypeDetector::select($this->empty_note));
+    }
+
+    /*public function test_note_type_detector_detects_images()
+    {
+
+    }*/
 }
