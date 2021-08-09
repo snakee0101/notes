1bfbb4de032b437040bb8fa67ec865d773737b47
Danil <snakee0101@gmail.com>
2021-08-09 19:03:26 +0300

    TagController::index - moved tag retrieving logic to the model

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -11,7 +11,7 @@ class TagController extends Controller
 {
     public function index()
     {
-        return auth()->user()->tags()->pluck('name');  //returns tag names
+        return Tag::getAllNames();
     }
 
     public function store(Request $request)
