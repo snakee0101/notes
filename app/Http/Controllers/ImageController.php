9ce85244573652a1183adcc98dfbd9c374d83f4f
Danil <snakee0101@gmail.com>
2021-08-11 12:32:48 +0300

    ImageController - replaced rights check with abort_if

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -18,8 +18,7 @@ class ImageController extends Controller
     {
         $note = Note::find( $request->input('note_id') );
 
-        if(Gate::denies('image_manipulation', $note))
-            return response('Only owner and collaborators can manipulate images', 403);
+        abort_if(Gate::denies('image_manipulation', $note), 403, 'Only owner and collaborators can manipulate images');
 
         $image = $request->file('image');
         $paths = Image::processUpload($image);
@@ -33,8 +32,7 @@ class ImageController extends Controller
 
     public function destroy(Image $image)
     {
-        if(Gate::denies('image_manipulation', $image->note))
-            return response('Only owner and collaborators can manipulate images', 403);
+        abort_if(Gate::denies('image_manipulation', $image->note), 403, 'Only owner and collaborators can manipulate images');
 
         $image_id = $image->id;
         $image->delete();
@@ -46,8 +44,7 @@ class ImageController extends Controller
     {
         $image = Image::withTrashed()->findOrFail($image_id);
 
-        if(Gate::denies('image_manipulation', $image->note))
-            return response('Only owner and collaborators can manipulate images', 403);
+        abort_if(Gate::denies('image_manipulation', $image->note), 403, 'Only owner and collaborators can manipulate images');
 
         $image->restore();
 
@@ -57,8 +54,8 @@ class ImageController extends Controller
     public function recognize(Request $request)
     {
         $image = Image::where('image_path', $request->image_path)->first();
-        if(Gate::denies('image_manipulation', $image->note))
-            return response('Only owner and collaborators can manipulate images', 403);
+
+        abort_if(Gate::denies('image_manipulation', $image->note), 403, 'Only owner and collaborators can manipulate images');
 
         $tesseract = new TesseractOCR(storage_path() . '/app/' . Str::after($request->image_path,'/storage'));
         return $tesseract->run();
