a83d036073ed1c302d3f88f1383d541839040752
Danil <snakee0101@gmail.com>
2021-08-14 13:58:28 +0300

    created container for search results

--- resources/js/components/SearchControlsComponent.vue
+++ resources/js/components/SearchControlsComponent.vue
@@ -1,47 +1,53 @@
 <template>
-    <div class="search-controls m-auto" style="max-width: 640px" :class="isSearchActive ? 'active' :''">
-        <div class="type-controls shadow-lg border border-gray-300 mb-4">
-            <h2 class="font-bold p-2 pb-4">Types</h2>
-            <div class="flex flex-row justify-between">
-                <button class="blue-search-box-button" @click="filterByType('checklist')">
-                    <i class="bi bi-list icon-lg text-white"></i>
-                    <p class="mt-10 mb-4">Lists</p>
-                </button>
-                <button class="blue-search-box-button" @click="filterByType('image')">
-                    <i class="bi bi-image icon-lg text-white"></i>
-                    <p class="mt-10 mb-4">Images</p>
-                </button>
-                <button class="blue-search-box-button" @click="filterByType('drawing')">
-                    <!--TODO: Drawings feature is not implemented yet-->
-                    <i class="bi bi-brush-fill icon-lg text-white"></i>
-                    <p class="mt-10 mb-4">Drawings</p>
-                </button>
-                <button class="blue-search-box-button" @click="filterByType('links')">
-                    <i class="bi bi-link icon-lg text-white"></i>
-                    <p class="mt-10 mb-4">URLs</p>
-                </button>
+    <div class="search-controls m-auto" :class="isSearchActive ? 'active' :''">
+        <div class="m-auto" style="max-width: 640px">
+            <div class="type-controls shadow-lg border border-gray-300 mb-4">
+                <h2 class="font-bold p-2 pb-4">Types</h2>
+                <div class="flex flex-row justify-between">
+                    <button class="blue-search-box-button" @click="filterByType('checklist')">
+                        <i class="bi bi-list icon-lg text-white"></i>
+                        <p class="mt-10 mb-4">Lists</p>
+                    </button>
+                    <button class="blue-search-box-button" @click="filterByType('image')">
+                        <i class="bi bi-image icon-lg text-white"></i>
+                        <p class="mt-10 mb-4">Images</p>
+                    </button>
+                    <button class="blue-search-box-button" @click="filterByType('drawing')">
+                        <!--TODO: Drawings feature is not implemented yet-->
+                        <i class="bi bi-brush-fill icon-lg text-white"></i>
+                        <p class="mt-10 mb-4">Drawings</p>
+                    </button>
+                    <button class="blue-search-box-button" @click="filterByType('links')">
+                        <i class="bi bi-link icon-lg text-white"></i>
+                        <p class="mt-10 mb-4">URLs</p>
+                    </button>
+                </div>
+            </div>
+            <div class="label-controls shadow-lg border border-gray-300 mb-4">
+                <h2 class="font-bold p-2 pb-4">Labels</h2>
+                <div class="flex flex-row items-start flex-wrap justify-between">
+                    <button class="gray-search-box-button mb-2" @click="filterByLabel(tag.name)" v-for="tag in tags_list">
+                        <i class="bi bi-tags-fill icon-lg"></i>
+                        <p class="mt-10 mb-4">{{ tag.name }}</p>
+                    </button>
+                </div>
+            </div>
+            <div class="color-controls shadow-lg border border-gray-300 mb-4">
+                <h2 class="font-bold p-2 pb-2">Colors</h2>
+                <div class="p-4 flex flex-row flex-wrap">
+                    <a v-for="color in colors" href=""
+                       class="color-circle"
+                       :class="'bg-google-' + color"
+                       v-b-tooltip.hover.bottom :title="color"
+                       @click.prevent="filterByColor(color)"></a>
+                </div>
             </div>
         </div>
-        <div class="label-controls shadow-lg border border-gray-300 mb-4">
-            <h2 class="font-bold p-2 pb-4">Labels</h2>
-            <div class="flex flex-row items-start flex-wrap justify-between">
-                <button class="gray-search-box-button mb-2" @click="filterByLabel(tag.name)" v-for="tag in tags_list">
-                    <i class="bi bi-tags-fill icon-lg"></i>
-                    <p class="mt-10 mb-4">{{ tag.name }}</p>
-                </button>
-            </div>
-        </div>
-        <div class="color-controls shadow-lg border border-gray-300 mb-4">
-            <h2 class="font-bold p-2 pb-2">Colors</h2>
-            <div class="p-4 flex flex-row flex-wrap">
-                <a v-for="color in colors" href=""
-                   class="color-circle"
-                   :class="'bg-google-' + color"
-                   v-b-tooltip.hover.bottom :title="color"
-                   @click.prevent="filterByColor(color)"></a>
-            </div>
+        <div class="searchResults">
+
         </div>
     </div>
+
 </template>
 
 <script>
