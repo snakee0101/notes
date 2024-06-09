1ef15942a7e86ea605797b906fd1834ed9575cdf
snakee0101 <snakee0101@gmail.com>
2024-06-09 11:21:18 +0000

    filtering results by color works

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,30 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\Note;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use MeiliSearch\MeiliSearch;
 
 class SearchController extends Controller
 {
+ /*function (SearchIndex $algolia, string $query, array $options) {
+        $options['body']['query']['bool']['filter']['geo_distance'] = [
+            'distance' => '1000km',
+            'location' => ['lat' => 36, 'lon' => 111],
+        ];
+
+        return $algolia->search($query, $options);
+    }
+)->get();*/
+
     public function __invoke(Request $request)
     {
         $searchQuery = $request->input('query');
-        return Note::search($searchQuery, function(\MeiliSearch\Endpoints\Indexes $engine, $query, array $options) {
-            $engine->updateAttributesForFaceting(['tags', 'type']);
+        return Note::search($searchQuery, function(\Meilisearch\Endpoints\Indexes $meilisearch_index, $query, array $options) {
+            $meilisearch_index->updateFilterableAttributes(['tags', 'type', 'color']);
 
             if(request()->has('filterBy') && request()->input('filterBy') === 'color') {
-                $options['filters'] = 'color=' . request('filterValue');
+                $options['filter'] = 'color = ' . request('filterValue');
             }
 
             if(request()->has('filterBy') && request()->input('filterBy') === 'tag') {
-                $options['facetFilters'] = ['tags:' . request()->input('filterValue')];
+                $options['facetFilters'] = ['tags:' . request('filterValue')];
             }
 
             if(request()->has('filterBy') && request()->input('filterBy') === 'type') {
-                $options['facetFilters'] = ['type:' . request()->input('filterValue')];
+                $options['facetFilters'] = ['type:' . request('filterValue')];
             }
 
-            return $engine->search($query, $options);
-        })->paginate();
+            return $meilisearch_index->search($query, $options);
+        })->paginate(1);
     }
 }
