1f07c463ad867235b96ea994dc2a095de09afa6d
snakee0101 <snakee0101@gmail.com>
2024-06-14 18:39:28 +0000

    testing - note could be searched by header and body

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -22,37 +22,35 @@ class SearchTest extends TestCase
     {
         parent::setUp();
 
-        Storage::fake();
-        Storage::makeDirectory('images');
-        Storage::makeDirectory('thumbnails_large');
-        Storage::makeDirectory('thumbnails_small');
-
         config(['scout.driver' => 'meilisearch']);
     }
 
     public function test_a_note_could_be_searched_by_header()
     {
-        $note = Note::factory()->create(['header' => 'test header']);
-        $note2 = Note::factory()->create(['header' => 'header 2']);
+        $note = Note::factory()->create(['header' => 'test header', 'id' => 100_000_000]);
+        $note2 = Note::factory()->create(['header' => 'header 2', 'id' => 100_000_001]);
 
         auth()->login($note->owner);
 
         $json = $this->post(route('search'), [
             'query' => 'test'
-        ])->assertOk()->content();
+        ])->content();
 
         $data = json_decode($json);
 
-        $this->assertEquals(1, $data->current_page);
-        $this->assertCount(1, $data->data);
-        $this->assertStringContainsString('test', $data->data[0]->header);
+        $note->unsearchable();
+        $note2->unsearchable();
+
+        $this->assertCount(1, $data);
+        $this->assertStringContainsString('test', $data[0]->header);
     }
 
     public function test_a_note_body_in_search_index_must_have_tags_stripped()
     {
-        $note = Note::factory()->create(['body' => "<div>some text in <b>HTML</b><!--tags--></div>"]);
+        $note = Note::factory()->create(['body' => "<div>some text in <b>HTML</b><!--tags--></div>", 'id' => 100_000_000]);
 
         $serialized_note = $note->toSearchableArray();
+        $note->unsearchable();
 
         $this->assertArrayHasKey('body', $serialized_note);
         $this->assertEquals('some text in HTML', $serialized_note['body']);
@@ -60,24 +58,30 @@ class SearchTest extends TestCase
 
     public function test_a_note_could_be_searched_by_body()
     {
-        $note = Note::factory()->create(['body' => 'note body']);
-        $note2 = Note::factory()->create(['body' => 'found']);
+        $note = Note::factory()->create(['body' => '<div>note <b>body</b></div>', 'id' => 100_000_000]);
+        $note2 = Note::factory()->create(['body' => 'found', 'id' => 100_000_001]);
 
         auth()->login($note->owner);
 
         $json = $this->post(route('search'), [
             'query' => 'note'
-        ])->assertOk()->content();
+        ])->content();
+
+        $note->unsearchable();
+        $note2->unsearchable();
 
         $data = json_decode($json);
-        $this->assertCount(1, $data->data);
-        $this->assertStringContainsString('note', $data->data[0]->body);
+
+        $this->assertCount(1, $data);
+        $this->assertEquals($note->body, $data[0]->body);
     }
 
     public function test_serialized_note_must_include_its_color()
     {
-        $note = Note::factory()->create(['color' => 'orange']);
+        $note = Note::factory()->create(['color' => 'orange', 'id' => 100_000_000]);
         $serialized_note = $note->toSearchableArray();
+
+        $note->unsearchable();
 
         $this->assertArrayHasKey('color', $serialized_note);
         $this->assertEquals('orange', $serialized_note['color']);
