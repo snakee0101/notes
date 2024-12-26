d6afcdfd464bc21f457d817588852bcbf0af1ac6
snakee0101 <snakee0101@gmail.com>
2024-12-26 17:05:09 +0200

    you can search for notes shared with you

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,11 +10,13 @@ class SearchController extends Controller
     public function __invoke(Request $request)
     {
         return Note::search($request->input('query'), function(\Meilisearch\Endpoints\Indexes $meilisearch_index, $query, array $options) use ($request) {
-            $meilisearch_index->updateFilterableAttributes(['tags', 'type', 'color']);
+            $meilisearch_index->updateFilterableAttributes(['tags', 'type', 'color', 'owners']);
 
             if($request->filled('filterBy')) {
-                $options['filter'] = "$request->filterBy = '$request->filterValue'";
+                $options['filter'] = ["$request->filterBy = '$request->filterValue'"];
             }
+
+            $options['filter'][] = "owners = " . auth()->id();  // seach only notes where you are the owner or collaborator
 
             return $meilisearch_index->search($query, $options);
         })->get();
